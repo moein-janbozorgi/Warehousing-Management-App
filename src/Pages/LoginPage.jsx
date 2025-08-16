ebe8fdd030885a02025cc06e0c7e5d1d43580f29
moein-janbozorgi <moeinjanb84@gmail.com>
2025-08-16 12:26:21 +0330

    loginpage styles added

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import styles from "./LoginPage.module.css";
+
 function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div className={styles.container}>
+      <h1>بوت‌کمپ‌بوتواستارت</h1>
+      <div className={styles.form}>
+        <img src="/images/Union.png" alt="botostart-logo" />
+        <h2>فرم ورود </h2>
+        <form>
+          <input type="text" placeholder="نام کاربری" />
+          <input type="password" placeholder=" رمز عبور" />
+        </form>
+        <button>ورود </button>
+        <Link to="/signup">ایجاد حساب کاربری!</Link>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
